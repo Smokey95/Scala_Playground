5c4f194fef3933e549c3be244f5618eb6bb77a3a
Smokey95 <simon.htwg@gmail.com>
2022-09-16 12:08:25 +0200

    Add first tests

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,9 +1,12 @@
-// For more information on writing tests, see
-// https://scalameta.org/munit/docs/getting-started.html
-class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+
+class TicTacToeSpec extends AnyWordSpec:
+  "TicTacToe" should {
+    "have a bar as String of form '+---+---+---+'" in {
+      bar() should be("+---+---+---+" + eol)
+    }
+    "have cells as String of form '|   |   |   |'" in {
+      cells() should be("|   |   |   |" + eol)
+    }
   }
-}
