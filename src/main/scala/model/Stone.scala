2f95f7b59ead5877b8408fb04a7f8369d1779553
Smokey95 <simon.htwg@gmail.com>
2022-09-17 10:55:41 +0200

    Add Stone enum

---
+++ src/main/scala/model/Stone.scala
@@ -0,0 +1,10 @@
+package model
+
+/*
+* Enum declaration for the TicTacToe stones
+*/
+enum Stone(stringRepresentation: String):
+  override def toString(): String = stringRepresentation
+  case X extends Stone("X")
+  case O extends Stone("O")
+  case Empty extends Stone(" ")
