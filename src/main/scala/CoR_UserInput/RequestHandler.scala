f12e91e3c4718bf40afdcd03493d88a3a4a4673c
Smokey95 <simon.htwg@gmail.com>
2022-10-18 09:23:10 +0200

    Try out the Chain of Responsibility pattern

---
+++ src/main/scala/CoR_UserInput/RequestHandler.scala
@@ -0,0 +1,38 @@
+package CoR_UserInput
+
+object RequestHandler {
+  
+  // Handler Interface
+  type RFRule = PartialFunction[Int, String]
+
+  // Concrete Handler
+  val developer: RequestHandler.Type = {
+    //println("developer called")
+    case req@FixBugRequest(desc) =>
+      println(s"I am a developer. I can fix this bug: $desc")
+      Response(req, handled = true)
+  }
+
+  // Concrete Handler
+  val architect: RequestHandler.Type = {
+    case req@FeatureRequest(desc) =>
+      println(s"I am a architect. I can implement this feature: $desc")
+      Response(req, handled = true)
+  }
+
+  // Concrete Handler
+  val CTO: RequestHandler.Type = {
+    case req@ProductRequest(desc) =>
+      println(s"I am a CTO. I can make this product: $desc")
+      Response(req, handled = true)
+  }
+
+  // Concrete Handler
+  val noOne: RequestHandler.Type = {
+    case req: Request =>
+      println("No one is responsible for this request.")
+      Response(req, handled = false)
+  }
+
+
+}
